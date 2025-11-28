74b2aa305262fd270bdada20dd1c985706bafece
ParhusipC <carlosparhusip03@gmail.com>
2025-11-28 15:33:20 +0700

    pembaruan repositori dengan file zip dan penghapusan file SQL

--- generate_hash.php
+++ generate_hash.php
@@ -1,6 +1,6 @@
 <?php
 // FILE: generate_hash.php
-$password_baru = 'hutabarat123'; // GANTI dengan password yang ingin Anda pakai!
+$password_baru = 'fenomeno1234'; // GANTI dengan password yang ingin Anda pakai!
 $hash_yang_benar = password_hash($password_baru, PASSWORD_DEFAULT);
 
 echo "Hash untuk password '{$password_baru}' adalah: <br><br>";
